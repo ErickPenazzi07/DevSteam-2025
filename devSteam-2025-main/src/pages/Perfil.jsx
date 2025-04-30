3caa6f9002282b5a3d66ecc5d77893e13a9848a7
PEDRO ARAUJO <mxpedro.araujo11@gmail.com>
2025-04-30 17:02:03 -0300

    deselvolvendo Editar Perfil

--- devSteam-2025-main/src/pages/Perfil.jsx
+++ devSteam-2025-main/src/pages/Perfil.jsx
@@ -2,9 +2,75 @@ import React from "react";
 
 const Perfil = () => {
   return (
-    <div>
-   
-      
+    <div className="container py-5 w-50">
+      <div className="min-h-screen flex items-center justify-center bg-[#2f4d6c]">
+        <h1 className="">Editar Perfil</h1>
+        <div className="container py-5 w-50">
+          <form>
+            
+            <div className="mb-">
+              <label className="form-label" htmlFor="frmNome">
+                Nome
+              </label>
+              <input
+                className="form-control"
+                type="text"
+                name="frmNome"
+                id="frmNome"
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="frmNome">
+                Data de Nascimento
+              </label>
+              <input
+                className="form-control"
+                type="DataNascimento"
+                name="frmDataNascimento"
+                id="frmDataNascimento"
+              />
+            </div>
+            <div className="mb-3">
+              <label className="form-label" htmlFor="frmEmail">
+                Número de telefone
+              </label>
+              <input
+                className="form-control"
+                type="telefone"
+                name="frmtelefone"
+                id="frmtelefone"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label" htmlFor="frmEmail">
+                CPF
+              </label>
+              <input
+                className="form-control"
+                type="CPF"
+                name="frmCPF"
+                id="frmCPF"
+              />
+            </div>
+
+            <div className="mb-3">
+              <label className="form-label" htmlFor="frmEmail">
+                E-mail
+              </label>
+              <input
+                className="form-control"
+                type="email"
+                name="frmEmail"
+                id="frmEmail"
+              />
+            </div>
+            <div className="d-flex justify-content-center">
+              <button className=" btn btn-primary w-25">Salvar</button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
