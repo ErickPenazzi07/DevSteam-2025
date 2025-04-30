9c22ec1213471ff52716f4d81724cb39d3e1aaed
PEDRO ARAUJO <mxpedro.araujo11@gmail.com>
2025-04-30 11:44:00 -0300

    Footer.jsx

---
+++ devSteam-2025-main/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="d-flex justify-content-around text-lg-start bg-dark text-light mt-5">
+      <div className=" d-flex  p-3">Feito por Garotos de Programa</div>
+      <div className="p-2">Precisa de ajuda? Fale com a gente!
+      📧 suporte@seudominio.com</div>
+    </footer>
+  );
+};
+
+export default Footer;
