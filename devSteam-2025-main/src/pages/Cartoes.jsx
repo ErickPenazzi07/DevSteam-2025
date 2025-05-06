c1484e8f6df7fbb366af1850281d4971bb1b77d9
DevErick <djerickdev@gmail.com>
2025-05-06 14:56:52 -0300

    ajustes cartões - profCastello

--- devSteam-2025-main/src/pages/Cartoes.jsx
+++ devSteam-2025-main/src/pages/Cartoes.jsx
@@ -2,80 +2,98 @@ import React from "react";
 import "../App.css";
 import HeaderSimples from "../components/HeaderSimples";
 
-
 const Cartoes = () => {
   return (
     <>
-    <HeaderSimples />
-    <div className=" card CardCartao d-flex border-3 border-black col-lg-6 col-md-8 col-sm-12 w-75    ">
-      <div className="container py-5 w-50">
-        <form>
-          <div className="mb-3">
+      <HeaderSimples />
+      <div className="vh-100 d-flex justify-content-center align-items-center">
+        <div className="card w-75 CardCartao d-flex border-3 border-black rounded-5">
+          <div className="p-5">
             <h1 className="text-center text">Cadastrar Cartão</h1>
-            <label className="form-label py-2 " htmlFor="frmNome">
-              Numero Cartão:
-            </label>
-            <input
-              className="form-control bg-transparent border-black"
-              type="text"
-              name="frmNome"
-              id="frmNome"
-            />
-          </div>
+            <form>
+              <div className="row">
+                <div className="mb-3 col-6">
+                  <label className="form-label" htmlFor="frmNome">
+                    Numero Cartão:
+                  </label>
+                  <input
+                    className="form-control bg-transparent border-black"
+                    type="text"
+                    name="frmNome"
+                    id="frmNome"
+                  />
+                </div>
 
-          <div className="mb-3">
-            <label className="form-label " htmlFor="frmEmail">
-              Nome do Titular:
-            </label>
-            <input
-              className="form-control bg-transparent border-black"
-              type="email"
-              name="frmEmail"
-              id="frmEmail"
-            />
+                <div className="mb-3 col-6">
+                  <label className="form-label" htmlFor="frmEmail">
+                    Nome do Titular:
+                  </label>
+                  <input
+                    className="form-control bg-transparent border-black"
+                    type="email"
+                    name="frmEmail"
+                    id="frmEmail"
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-8">
+                  <div className="row">
+                    <div className="mb-3 col-6">
+                      <label className="form-label" htmlFor="frmValidade">
+                        Validade:
+                      </label>
+                      <input
+                        className="form-control bg-transparent border-black"
+                        type="text"
+                        name="frmValidade"
+                        id="frmValidade"
+                      />
+                    </div>
+                    <div className="mb-3 col-6">
+                      <label className="form-label" htmlFor="frmCVV">
+                        CVV:
+                      </label>
+                      <input
+                        className="form-control bg-transparent border-black"
+                        type="text"
+                        name="frmCVV"
+                        id="frmCVV"
+                      />
+                    </div>
+                    <div className="mb-3 col-12">
+                      <label className="form-label" htmlFor="frmCPF">
+                        CPF:
+                      </label>
+                      <input
+                        className="form-control bg-transparent border-black"
+                        type="text"
+                        name="frmCPF"
+                        id="frmCPF"
+                      />
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-end gap-3">
+                    <button className="btn btn-primary w-50 my-2">
+                      Cadastrar Cartão
+                    </button>
+                    <button className="btn btn-danger w-50 my-2">
+                      Cancelar
+                    </button>
+                  </div>
+                </div>
+                <div className="col-4 d-flex align-items-center">
+                  <img
+                    className="img-fluid"
+                    src="https://placehold.co/300x180"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </form>
           </div>
-            <div className="mb-3">
-                <label className="form-label " htmlFor="frmEmail">
-                Validade:
-                </label>
-                <input
-                className="form-control bg-transparent border-black"
-                type="email"
-                name="frmEmail"
-                id="frmEmail"
-                />
-            </div>
-            <div className="mb-3">
-                <label className="form-label " htmlFor="frmEmail">
-                CVV:
-                </label>
-                <input
-                className="form-control bg-transparent border-black"
-                type="email"
-                name="frmEmail"
-                id="frmEmail"
-                />
-            </div>
-            <div className="mb-3">
-                <label className="form-label " htmlFor="frmEmail">
-                CPF:
-                </label>
-                <input
-                className="form-control bg-transparent border-black"
-                type="email"
-                name="frmEmail"
-                id="frmEmail"
-                />
-            </div>
-
-          <div className="d-flex justify-content-end">
-            <button className="btn btn-primary w-50 m-2 ">Cadastrar Cartão</button>
-            <button className="btn btn-danger w-50 m-2 ">Cancelar</button>
-          </div>
-          
-        </form>
+        </div>
       </div>
-    </div>
     </>
   );
 };
